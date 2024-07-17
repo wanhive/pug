68be6413bb0a34b1d9fabaf599623afee507b552
Amit Kumar <amitkriit@gmail.com>
2024-07-17 19:46:01 +0530

    Add copyright information

--- src/wanhive-pug.h
+++ src/wanhive-pug.h
@@ -1,8 +1,23 @@
 /*
  * wanhive-pug.h
  *
- *  Created on: Jul 17, 2024
- *      Author: amit
+ * Copyright (C) 2024 Amit Kumar (amitkriit@gmail.com)
+ *
+ * SPDX License Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef WANHIVE_PUG_H_

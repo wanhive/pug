e63fa40affa9787f38bba2d3d7c14cacda0ad5b5
Amit Kumar <amitkriit@gmail.com>
2024-07-18 08:09:02 +0530

    PCA9685 driver implementation
    PCA9685 driver for 12-bit resolution PWM and Servo application.

---
+++ src/driver/PCA9685.cpp
@@ -0,0 +1,226 @@
+/*
+ * PCA9685.cpp
+ *
+ * Copyright (C) 2024 Wanhive Systems Private Limited (info@wanhive.com)
+ *
+ * SPDX License Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "PCA9685.h"
+#include <wanhive/base/ds/Twiddler.h>
+#include <wanhive/base/Timer.h>
+
+namespace {
+
+constexpr unsigned char MODE1_REG = 0x0;
+constexpr unsigned char MODE2_REG = 0x01;
+
+constexpr unsigned char RESTART_MASK = 0x80;
+constexpr unsigned char SLEEP_MASK = 0x10;
+constexpr unsigned char AI_MASK = 0x20;
+
+constexpr unsigned char INVRT_MASK = 0x10;
+
+constexpr unsigned char FULL_MASK = 0x10;
+
+/**
+ * Locates the correct LEDX_ON_L register for the pin number starting at 0.
+ */
+unsigned int baseRegister(unsigned int pin) {
+	constexpr unsigned int PIN_COUNT = 16; //Total number of pins
+	constexpr unsigned int LED0_ON_L = 0x6; //First LED
+	constexpr unsigned int ALL_LED_ON_L = 0xFA; //All LED
+	if (pin < PIN_COUNT) {
+		return LED0_ON_L + (pin * 4);
+	} else {
+		return ALL_LED_ON_L;
+	}
+}
+
+}  // namespace
+
+namespace wanhive {
+
+PCA9685::PCA9685(unsigned int bus, unsigned int device) :
+		SMBus(bus, device) {
+	setup();
+}
+
+PCA9685::PCA9685(const char *path, unsigned int device) :
+		SMBus(path, device) {
+	setup();
+}
+
+PCA9685::~PCA9685() noexcept {
+
+}
+
+void PCA9685::pwmWrite(unsigned int pin, unsigned int value) {
+	if (value >= PWM_MAX) {
+		fullOn(pin, true);
+	} else if (value > 0) {
+		write(pin, 0, value);
+	} else {
+		fullOff(pin, true);
+	}
+}
+
+void PCA9685::digitalWrite(unsigned int pin, bool value) {
+	if (value) {
+		fullOn(pin, true);
+	} else {
+		fullOff(pin, true);
+	}
+}
+
+unsigned int PCA9685::setFrequency(unsigned int frequency) {
+	/*
+	 * Using the internal oscillator
+	 * Limit the frequency to the range [MIN_FREQUENCY, MAX_FREQUENCY]
+	 * The data sheet says 24Hz to 1526Hz
+	 */
+	constexpr double OSCILLATOR_FREQUENCY = 25000000;
+	constexpr unsigned char PRESCALE_MIN = 0x03;
+	constexpr unsigned char PRESCALE_REG = 0xFE;
+	frequency = (
+			frequency < MIN_FREQUENCY ?
+					MIN_FREQUENCY :
+					(frequency > MAX_FREQUENCY ? MAX_FREQUENCY : frequency));
+	unsigned char prescale = (unsigned char) (((OSCILLATOR_FREQUENCY
+			/ (frequency * 4096)) + 0.5) - 1);
+
+	if (prescale < PRESCALE_MIN) { //Just in case
+		prescale = PRESCALE_MIN;
+	}
+
+	unsigned char state;
+	SMBus::read(MODE1_REG, state);
+	//Clear the restart bit
+	state &= ~RESTART_MASK;
+	//Go to sleep (set the sleep bit)
+	state |= SLEEP_MASK;
+	SMBus::write(MODE1_REG, state);
+	//Set prescale
+	SMBus::write(PRESCALE_REG, prescale);
+	//Wake up (clear the sleep bit)
+	state &= ~SLEEP_MASK;
+	SMBus::write(MODE1_REG, state);
+	//Allow the oscillator to stabilize
+	Timer::sleep(1);
+	//Restart PWM
+	state |= (RESTART_MASK | AI_MASK);
+	SMBus::write(MODE1_REG, state);
+
+	return frequency;
+}
+
+void PCA9685::restart() {
+	unsigned char state;
+	SMBus::read(MODE1_REG, state);
+	if (state & RESTART_MASK) {
+		state &= ~RESTART_MASK;
+		state &= ~SLEEP_MASK;
+		SMBus::write(MODE1_REG, state);
+		Timer::sleep(1);
+	}
+
+	state |= RESTART_MASK;
+	SMBus::write(MODE1_REG, state);
+}
+
+void PCA9685::sleep() {
+	unsigned char state;
+	SMBus::read(MODE1_REG, state);
+	state |= SLEEP_MASK;
+	SMBus::write(MODE1_REG, state);
+}
+
+void PCA9685::wakeUp() {
+	unsigned char state;
+	SMBus::read(MODE1_REG, state);
+	state &= ~SLEEP_MASK;
+	SMBus::write(MODE1_REG, state);
+	Timer::sleep(1);
+}
+
+void PCA9685::write(unsigned int pin, unsigned short on, unsigned short off) {
+	//Use only the lowest 12 bits, clear the full-on and full-off bits.
+	on &= 0x0FFF;
+	off &= 0x0FFF;
+
+	//Write to on and off registers
+	auto reg = baseRegister(pin);
+	SMBus::write(reg, on);      //LEDX_ON
+	SMBus::write(reg + 2, off); //LEDX_OFF
+}
+
+void PCA9685::read(unsigned int pin, unsigned short &on, unsigned short &off) {
+	auto reg = baseRegister(pin);
+	SMBus::read(reg, on);
+	SMBus::read(reg + 2, off);
+}
+
+void PCA9685::fullOn(unsigned int pin, bool flag) {
+	auto reg = baseRegister(pin) + 1; //LEDX_ON_H
+	unsigned char state;
+	SMBus::read(reg, state);
+	state = Twiddler::mask(state, FULL_MASK, flag);
+	SMBus::write(reg, state);
+
+	//Because full-off takes precedence
+	if (flag) {
+		fullOff(pin, false);
+	}
+}
+
+void PCA9685::fullOff(unsigned int pin, bool flag) {
+	auto reg = baseRegister(pin) + 3; //LEDX_OFF_H
+	unsigned char state;
+	SMBus::read(reg, state);
+	state = Twiddler::mask(state, FULL_MASK, flag);
+	SMBus::write(reg, state);
+}
+
+void PCA9685::setOutputMode(bool invert, bool openDrain) {
+	constexpr unsigned char TOTEMPOLE_MASK = 0x04;
+	unsigned char state;
+	SMBus::read(MODE2_REG, state);
+
+	if (invert) {
+		state |= INVRT_MASK;
+	} else {
+		state &= ~INVRT_MASK;
+	}
+
+	if (openDrain) {
+		state &= ~TOTEMPOLE_MASK;
+	} else {
+		state |= TOTEMPOLE_MASK;
+	}
+
+	SMBus::write(MODE2_REG, state);
+}
+
+void PCA9685::setup() {
+	unsigned char state;
+	SMBus::read(MODE1_REG, state);
+	//Enable register auto-increment
+	state = (state & ~RESTART_MASK) | AI_MASK;
+	SMBus::write(MODE1_REG, state);
+}
+
+} /* namespace wanhive */
---
+++ src/driver/PCA9685.h
@@ -0,0 +1,144 @@
+/*
+ * PCA9685.h
+ *
+ * Copyright (C) 2024 Wanhive Systems Private Limited (info@wanhive.com)
+ *
+ * SPDX License Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef WH_DRIVER_PCA9685_H_
+#define WH_DRIVER_PCA9685_H_
+#include "../physical/SMBus.h"
+
+namespace wanhive {
+/**
+ * User space PCA9685 driver.
+ * @note PCA9685 is a 12-bit PWM/Servo driver which will drive up to 16 servos
+ * with I2C. This module demonstrates the use of the SMBus interfaces.
+ * @ref http://www.nxp.com/documents/data_sheet/PCA9685.pdf
+ */
+class PCA9685: protected SMBus {
+public:
+	/**
+	 * Constructor: initializes a PCA9685 driver.
+	 * @param bus i2c adapter's identifier
+	 * @param device device identifier (typically 0x40)
+	 */
+	PCA9685(unsigned int bus, unsigned int device = 0x40);
+	/**
+	 * Constructor: initializes a PCA9685 driver.
+	 * @param path i2c adapter's pathname
+	 * @param device device identifier (typically 0x40)
+	 */
+	PCA9685(const char *path, unsigned int device = 0x40);
+	/**
+	 * Destructor: closes the i2c bus.
+	 */
+	~PCA9685() noexcept;
+	/**
+	 * Simple PWM control which sets the on-tick to 0 and the off-tick to a
+	 * given value.
+	 * @param pin the pin number (0-15), ALL_PIN to select all.
+	 * @param value the value to set. If the value is 0 then full-off will be
+	 * enabled. If the value is >= PWM_MAX then full-on will be enabled. Every
+	 * value in between enables the PWM output.
+	 */
+	void pwmWrite(unsigned int pin, unsigned int value);
+	/**
+	 * Simple full-on and full-off control.
+	 * @param pin the pin number (0-15), ALL_PIN to select all.
+	 * @param value if value is false, full-off will be enabled. If value is
+	 * true, full-on will be enabled.
+	 */
+	void digitalWrite(unsigned int pin, bool value);
+	/**
+	 * Sets the frequency of PWM signals (assumes internal oscillator).
+	 * @param frequency the frequency to set. Frequency will be capped to the
+	 * range [40, 1000] Hz.
+	 * @return the frequency been set
+	 */
+	unsigned int setFrequency(unsigned int frequency);
+	/**
+	 * Restarts the PCA9685 module.
+	 */
+	void restart();
+	/**
+	 * Puts the module into sleep mode.
+	 */
+	void sleep();
+	/**
+	 * Wakes up the module.
+	 */
+	void wakeUp();
+	/**
+	 * Writes the on and off ticks manually to a pin (deactivates full-on and
+	 * full-off).
+	 * @param pin the pin number (0-15), ALL_PIN to select all.
+	 * @param on the on tick
+	 * @param off the off tick
+	 */
+	void write(unsigned int pin, unsigned short on, unsigned short off);
+	/**
+	 * Reads both on and off registers as 16 bit of data. To get PWM mask each
+	 * value with 0xFFF. To get full-on or full-off bit mask with 0x1000.
+	 * @param pin the pin number (0-15), ALL_PIN to select all.
+	 * @param on the on register value
+	 * @param off the off register value
+	 */
+	void read(unsigned int pin, unsigned short &on, unsigned short &off);
+	/**
+	 * Activates or deactivates full-on.
+	 * @param pin the pin number (0-15), ALL_PIN to select all.
+	 * @param flag true for full-on, false for PWM
+	 */
+	void fullOn(unsigned int pin, bool flag);
+	/**
+	 * Activates or deactivates full-off.
+	 * @param pin the pin number (0-15), ALL_PIN to select all.
+	 * @param flag true for full-off, false for PWM or full-on
+	 */
+	void fullOff(unsigned int pin, bool flag);
+	/**
+	 * Sets up the output mode.
+	 * @param invert true to invert the output logic state, false otherwise
+	 * @param openDrain true for open drain, false for totem-pole
+	 */
+	void setOutputMode(bool invert, bool openDrain);
+private:
+	void setup();
+public:
+	/**
+	 * The maximum PWM value (exclusive).
+	 */
+	static constexpr unsigned int PWM_MAX = 4096;
+	/**
+	 * All pins selector.
+	 */
+	static constexpr unsigned int ALL_PIN = 16;
+	/**
+	 * Minimum allowed frequency.
+	 */
+	static constexpr unsigned int MIN_FREQUENCY = 40;
+	/**
+	 * Maximum allowed frequency.
+	 */
+	static constexpr unsigned int MAX_FREQUENCY = 1000;
+};
+
+} /* namespace wanhive */
+
+#endif /* WH_DRIVER_PCA9685_H_ */
